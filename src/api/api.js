2ed98f71b5fd039d6b3fed4161dd902f555266c2
David Bullado <david.bullado@gmail.com>
2018-10-23 15:25:54 +0200

    Api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
-// export var API_URL = 'https://api.dev.sarl:443'
-export var API_URL = 'http://127.0.0.1:5000'
+export var API_URL = 'https://api.dev.sarl:443'
+// export var API_URL = 'http://127.0.0.1:5000'
 
 export function getArticle (hlink) {
   return fetch(API_URL + '/article/' + hlink)
